1bb38dffe5b0f797afd217aedbd3ae73394f44ba
JShadwick <skadwick@gmail.com>
2025-02-03 17:46:23 -0500

    Initial commit

---
+++ TaskTracker_CRUD.Server/Data/TrackedTaskContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTracker_CRUD.Server.Models;
+
+namespace TaskTracker_CRUD.Server.Data
+{
+    public class TrackedTaskContext : DbContext
+    {
+        public TrackedTaskContext(DbContextOptions<TrackedTaskContext> options) : base(options) { }
+
+        public DbSet<TrackedTask> Examples { get; set; }
+    }
+}
---
+++ TaskTracker_CRUD.Server/Models/TrackedTask.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskTracker_CRUD.Server.Models
+{
+    public class TrackedTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
